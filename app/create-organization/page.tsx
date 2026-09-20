05b7adecc5c27b10d084b0efc96a7788684a8406
paramcodes <sparamveer1001@gmail.com>
2026-09-20 09:26:16 +0530

    refactor: move create-organization into (auth) group, add .dockerignore

--- app/create-organization/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateOrganization } from "@clerk/nextjs"
-
-export default function CreateOrganizationPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <CreateOrganization afterCreateOrganizationUrl="/" />
-    </div>
-  )
-}
