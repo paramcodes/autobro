d6e5767cfd6e8439dd5b8c74af7c5ae33f3aadee
paramcodes <sparamveer1001@gmail.com>
2026-09-20 21:32:30 +0530

    refactor: extract canvas div into WorkflowCanvas component

---
+++ features/workflows/components/workflow-canvas.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export function WorkflowCanvas() {
+  return (
+    <div className="flex size-full items-center justify-center">
+      Canvas
+    </div>
+  )
+}
--- features/workflows/components/workflow-shell.tsx
+++ features/workflows/components/workflow-shell.tsx
@@ -6,6 +6,7 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable"
 import { RightSidebar } from "./right-sidebar"
+import { WorkflowCanvas } from "./workflow-canvas"
 
 export function WorkflowShell({ workflowId }: { workflowId: string }) {
   return (
@@ -17,9 +18,7 @@ export function WorkflowShell({ workflowId }: { workflowId: string }) {
       <ResizablePanel minSize="30rem">
         <ResizablePanelGroup orientation="vertical" className="size-full">
           <ResizablePanel minSize="18rem">
-            <div className="flex size-full items-center justify-center">
-              Canvas
-            </div>
+            <WorkflowCanvas />
           </ResizablePanel>
           <ResizableHandle />
           <ResizablePanel defaultSize="8rem" minSize="6rem">
